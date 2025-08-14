1d01eca9829fbef49929a8bc98e631632d485172
jeankarlo762 <jeankarlomaykessler123@gmail.com>
2025-08-13 23:13:09 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/demo/SistemaFoodTruckApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFoodTruckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFoodTruckApplication.class, args);
+	}
+
+}
